3c2be0b3e3182c45834d1bae109860ed42052fef
jamesallison <jamesallison.uk@gmail.com>
2017-07-04 19:32:04 +0100

    webhook basic

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 func main() {
@@ -15,14 +14,11 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	http.HandleFunc("/webhook", webhookHandler)
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	http.ListenAndServe(":" + port, nil)
+}
 
-	router.Run(":" + port)
+func webhookHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello from webhook!")
 }
